7b5b059d113b2a92e9f52f64b510c039bee1b16d
vzhufk <vzhufk@gmail.com>
2018-12-12 11:53:27 +0200

    [:tada:] Comments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181212091256) do
+ActiveRecord::Schema.define(version: 20181212094218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20181212091256) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -30,4 +40,5 @@ ActiveRecord::Schema.define(version: 20181212091256) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "comments", "articles"
 end
